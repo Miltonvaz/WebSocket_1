87e4fdb095d8184b74616c1e015bb3f2774202c1
Miltonvaz <233427@ids.upchiapas.edu.mx>
2025-07-23 22:49:35 -0600

    Update

--- src/domain/entities/Sensors.go
+++ src/domain/entities/Sensors.go
@@ -4,9 +4,10 @@ type Message struct {
 	IdUser        int     `json:"id_user"`
 	Code          int     `json:"code"`
 	PH            float64 `json:"ph"`
-	Conductividad int     `json:"conductividad"`
-	Turbuidez     int     `json:"turbuidez"`
+	Conductividad float64 `json:"conductividad"`
+	Turbuidez     float64 `json:"turbuidez"`
 	Temperatura   float64 `json:"temperatura"`
-	Alcohol       int     `json:"alcohol"`
-	Densidad      int     `json:"densidad"`
+	Alcohol       float64 `json:"alcohol"`
+	Densidad      float64 `json:"densidad"`
+	Rpm           int     `json:"rpm"`
 }
